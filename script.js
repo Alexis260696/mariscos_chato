812fb09dfd4c484038757602bf136c86927ddd7b
Derian Alexis <alexian260696@gmail.com>
2026-02-05 00:14:26 -0600

    finish

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Menú cargado correctamente");
 });
+
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollTopBtn.style.display = 'block';
+    } else {
+        scrollTopBtn.style.display = 'none';
+    }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
+
